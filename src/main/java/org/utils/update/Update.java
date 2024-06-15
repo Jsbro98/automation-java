021967026abd7a5fc1196ce2a8405c2b80334812
Jacob <jacobstanbrough@gmail.com>
2024-06-14 23:45:03 -0500

    created update script

---
+++ src/main/java/org/utils/update/Update.java
@@ -0,0 +1,28 @@
+package org.utils.update;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Update {
+  public static void main(String[] args) {
+    ProcessBuilder procBuilder = new ProcessBuilder();
+    procBuilder.command("sudo", "apt", "update");
+    procBuilder.command("sudo", "apt", "upgrade");
+    procBuilder.directory(new File(System.getProperty("user.home")));
+    try {
+      Process process = procBuilder.start();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+      }
+
+      reader.close();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
